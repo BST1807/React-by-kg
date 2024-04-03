4c6c325f97b9049ab27b422e2dbcf23f3b826f53
BST1807 <bhaveshsingh1807@gmail.com>
2024-04-03 20:41:34 +0530

    second commit

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -5,11 +5,14 @@ import TodoItem2 from "./components/TodoItem2";
 import './App.css';
 function App() {
   return (
-    <center class="todo-container">
+    <center className="todo-container">
       <AppName></AppName>
       <AddTodo></AddTodo>
+      <div className='items-container'>
       <TodoItem1></TodoItem1>
       <TodoItem2></TodoItem2>
+      </div>
+     
     </center>
   );
 }
